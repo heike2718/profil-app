a922df12bee2a5a372e5093e40c40d247d97065c
Heike Winkelvoß <public@egladil.de>
2019-08-11 18:55:52 +0200

    release 2.0.0

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
 	production: true,
-	version: '1.0.0',
+	version: '2.0.0',
 	envName: 'Production',
 	apiUrl: 'https://opa-wetterwachs.de/authprovider',
 	authUrl: 'https://opa-wetterwachs.de/auth-app',
