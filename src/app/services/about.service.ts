a19517b8182fb5b4750261b5ef58cc435f4e02c2
Heike Winkelvoß <public@egladil.de>
2020-03-18 08:13:49 +0100

    about section with server version

---
+++ src/app/services/about.service.ts
@@ -0,0 +1,29 @@
+import { store } from '../shared/store/app-data';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { ResponsePayload } from 'hewi-ng-lib';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+	providedIn: 'root'
+})
+export class AboutService {
+
+	constructor(private httpClient: HttpClient) { }
+
+	loadAboutInfos(): void {
+
+		const url = environment.apiUrl + '/version';
+
+		this.httpClient.get(url).pipe(
+			map(res => res as ResponsePayload)).subscribe(
+				pl => {
+					store.updateApiVersion(pl.message.message);
+				},
+				(_error => {
+					store.updateApiVersion('Der Server ist nicht erreichbar :/');
+				}));
+
+	}
+}
